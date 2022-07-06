df3131de437c16250fa690659d91b195082f0393
Jakub Vondráček <jakub.vondracek@sense4code.com>
2022-07-06 09:07:17 +0200

    Add project files.

---
+++ HangfireTest.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using HangfireTest.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HangfireTest.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable(nameof(User));
+        }
+    }
+}
